4ab1336d2f16139d97d1d8172245a80f2df4d0e8
tawan158 <tawan158@ugm.com.tw>
2020-02-15 13:04:28 +0800

    20200215_1

---
+++ user.php
@@ -0,0 +1,38 @@
+<?php
+/* 引入檔頭，每支程都會引入 */
+require_once 'head.php';
+ 
+//if(!$_SESSION['admin'])redirect_header("index.php", '您沒有權限', 3000);
+
+/* 過濾變數，設定預設值 */
+$op = system_CleanVars($_REQUEST, 'op', 'op_list', 'string');
+$sn = system_CleanVars($_REQUEST, 'sn', '', 'int');
+// echo $op;die();
+ 
+/* 程式流程 */
+switch ($op){
+
+ 
+  default:
+    $op = "op_list";
+    op_list();
+    break;  
+}
+/*---- 將變數送至樣版----*/
+$smarty->assign("WEB", $WEB);
+$smarty->assign("op", $op);
+ 
+/*---- 程式結尾-----*/
+$smarty->display('admin.tpl');
+ 
+/*---- 函數區-----*/
+
+
+
+function op_list(){
+  global $smarty,$db;
+  
+}
+
+
+
